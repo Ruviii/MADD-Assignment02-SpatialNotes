f965b6306e8f83005175571bc8f53ecd3349591e
IT24103033 <it24103033@my.sliit.lk>
2025-11-17 07:57:58 +0530

    Initial Commit

---
+++ SpatialNotesTests/SpatialNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialNotesTests.swift
+//  SpatialNotesTests
+//
+//  Created by Sujana Dinuwara on 2025-11-17.
+//
+
+import Testing
+@testable import SpatialNotes
+
+struct SpatialNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
